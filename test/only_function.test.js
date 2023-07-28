834c3211d5be0a7562271a927f05b066c7fc3193
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-07-28 11:33:16 +0700

    only Function

---
+++ test/only_function.test.js
@@ -0,0 +1,5 @@
+test('Test 1', () => console.log('Test 1'));
+test('Test 2', () => console.log('Test 2'));
+test.only('Test 3', () => console.log('Test 3'));
+test('Test 4', () => console.log('Test 4'));
+test('Test 5', () => console.log('Test 5'));
